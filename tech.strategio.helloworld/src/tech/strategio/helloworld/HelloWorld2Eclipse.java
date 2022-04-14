bca7a9d6b2affd226ba9e89c3708024cf8c2fdd8
Susan Choi <itsmesusanee@gmail.com>
2022-04-14 16:30:27 -0400

    committing first time hello World 2

---
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorld2Eclipse.java
@@ -0,0 +1,11 @@
+package tech.strategio.helloworld;
+
+public class HelloWorld2Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Demo 2 With the Team");
+
+	}
+
+}
